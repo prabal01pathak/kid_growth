e23d651ead5787736098961181f018b3dd9b15a7
Prabal Pathak <prabal01pathak@gmail.com>
2022-03-01 01:44:37 +0530

    modify settings.py

--- growth/growth/settings.py
+++ growth/growth/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static/']
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
