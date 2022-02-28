1ef26f558dc7a0b7d940d9b5a7ec7cafdd518215
Prabal Pathak <prabal01pathak@gmail.com>
2022-03-01 02:27:32 +0530

    fixing staticfiles

--- growth/settings.py
+++ growth/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / 'static/'
 STATIC_URL = 'static/'
-#STATICFILES_DIRS = [BASE_DIR / 'static/']
+STATICFILES_DIRS = [BASE_DIR / 'static/']
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
