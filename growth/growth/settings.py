56d5bf6b6ec3846655add02fd8944a01589b2fef
Prabal Pathak <prabal01pathak@gmail.com>
2022-03-01 02:00:32 +0530

    add static files

--- growth/growth/settings.py
+++ growth/growth/settings.py
@@ -123,8 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'static/'
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static/']
+#STATICFILES_DIRS = [BASE_DIR / 'static/']
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
